68fea9c726e7c4fa43a131ee46dd6e9b2f9795d0
James Houlahan <james.houlahan@proton.ch>
2023-02-21 21:29:26 +0100

    feat: Trash/Delete folder children

--- link.go
+++ link.go
@@ -19,3 +19,31 @@ func (c *Client) GetLink(ctx context.Context, shareID, linkID string) (Link, err
 
 	return res.Link, nil
 }
+
+func (c *Client) CreateFile(ctx context.Context, shareID string, req CreateFileReq) (CreateFileRes, error) {
+	var res struct {
+		File CreateFileRes
+	}
+
+	if err := c.do(ctx, func(r *resty.Request) (*resty.Response, error) {
+		return r.SetResult(&res).SetBody(req).Post("/drive/shares/" + shareID + "/files")
+	}); err != nil {
+		return CreateFileRes{}, err
+	}
+
+	return res.File, nil
+}
+
+func (c *Client) CreateFolder(ctx context.Context, shareID string, req CreateFolderReq) (CreateFolderRes, error) {
+	var res struct {
+		Folder CreateFolderRes
+	}
+
+	if err := c.do(ctx, func(r *resty.Request) (*resty.Response, error) {
+		return r.SetResult(&res).SetBody(req).Post("/drive/shares/" + shareID + "/folders")
+	}); err != nil {
+		return CreateFolderRes{}, err
+	}
+
+	return res.Folder, nil
+}
