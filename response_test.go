67859aec0317ada69a4c66e61c1d1889a0471510
Leander Beernaert <leander.beernaert@proton.ch>
2023-06-05 09:38:48 +0200

    feat(GODT-2653): Include optional Details field in APIError

--- response_test.go
+++ response_test.go
@@ -2,6 +2,7 @@ package proton_test
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"net"
 	"net/url"
@@ -34,4 +35,39 @@ func TestNetError_DropOnWrite(t *testing.T) {
 	if urlErr := new(url.Error); !errors.As(pingErr, &urlErr) {
 		t.Fatalf("expected a url.Error, got %T: %v", pingErr, pingErr)
 	}
+}
+
+func TestAPIError_DeserializeWithoutDetails(t *testing.T) {
+	errJson := `
+{
+	"Status": 400,
+	"Code": 1000,
+	"Error": "Foo Bar"
+}
+`
+	var err proton.APIError
+
+	require.NoError(t, json.Unmarshal([]byte(errJson), &err))
+	require.Nil(t, err.Details)
+}
+
+func TestAPIError_DeserializeWithDetails(t *testing.T) {
+	errJson := `
+{
+	"Status": 400,
+	"Code": 1000,
+	"Error": "Foo Bar",
+	"Details": {
+		"object2": {
+			"v": 20
+		},
+		"foo": "bar"
+	}
+}
+`
+	var err proton.APIError
+
+	require.NoError(t, json.Unmarshal([]byte(errJson), &err))
+	require.NotNil(t, err.Details)
+	require.Equal(t, `{"foo":"bar","object2":{"v":20}}`, err.DetailsToString())
 }
