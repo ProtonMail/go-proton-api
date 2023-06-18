213d1b4cac3d305743a206309c57a32ebcfd8557
Xavier Michelon <xavier.michelon@proton.ch>
2023-06-18 09:06:35 +0200

    feat: updated Drive's volume & share data types.

--- volume_types.go
+++ volume_types.go
@@ -4,13 +4,16 @@ package proton
 type Volume struct {
 	VolumeID string // Encrypted volume ID
 
-	CreateTime int64 // Creation time of the volume in Unix time
-	ModifyTime int64 // Last modification time of the volume in Unix time
-	MaxSpace   int64 // Space limit for the volume in bytes
-	UsedSpace  int64 // Space used by files in the volume in bytes
+	CreationTime    int64  // Creation time of the volume in Unix time
+	ModifyTime      int64  // Last modification time of the volume in Unix time
+	MaxSpace        *int64 // Space limit for the volume in bytes, null if unlimited.
+	UsedSpace       int64  // Space used by files in the volume in bytes
+	DownloadedBytes int64  // The amount of downloaded data since last reset
+	UploadedBytes   int64  // The amount of uploaded data since the last reset
 
-	State VolumeState // The state of the volume (active, locked, maybe more in the future)
-	Share VolumeShare // The main share of the volume
+	State         VolumeState          // The state of the volume (active, locked, maybe more in the future)
+	Share         VolumeShare          // The main share of the volume
+	RestoreStatus *VolumeRestoreStatus // The status of the restore task. Null if not applicable
 }
 
 // VolumeShare is the main share of a volume.
@@ -25,4 +28,13 @@ type VolumeState int
 const (
 	VolumeStateActive VolumeState = 1
 	VolumeStateLocked VolumeState = 3
+)
+
+// VolumeRestoreStatus is the status of the restore task.
+type VolumeRestoreStatus int
+
+const (
+	RestoreStatusDone       VolumeRestoreStatus = 0
+	RestoreStatusInProgress VolumeRestoreStatus = 1
+	RestoreStatusFailed     VolumeRestoreStatus = -1
 )
