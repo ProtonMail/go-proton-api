7b964c02e1fe7432dd415be1eea648e7e863cdb4
Xavier Michelon <xavier.michelon@proton.ch>
2024-03-24 19:48:06 +0100

    Update and document CreateFolder request and response.

--- link_folder_types.go
+++ link_folder_types.go
@@ -1,20 +1,23 @@
 package proton
 
+// CreateFolderReq contains the fields required for creating a folder. All PGP keys and signatures are in armored format.
 type CreateFolderReq struct {
-	ParentLinkID string
+	ParentLinkID string // The link ID of the parent folder.
 
-	Name string
-	Hash string
+	Name string // The folder name. PGP message. encrypted using the parent folder node key, signed using the address key.
+	Hash string // The folder name hash as a hex string, hashed using NodeHashKey.
 
-	NodeKey     string
-	NodeHashKey string
+	NodeKey                 string // The new node key. PGP message. Not signed.
+	NodePassphrase          string // The node passphrase. PGP message. Encrypted with the parent's node key.
+	NodePassphraseSignature string // The node passphrase signature. PGP message. Signed using the address key.
+	NodeHashKey             string // The new node hash key .PGP message. Encrypted and signed using the node key.
 
-	NodePassphrase          string
-	NodePassphraseSignature string
+	SignatureAddress string // The signature address used to sign passphrase and name.
 
-	SignatureAddress string
+	XAttr *string // The optional extended attributes. PGP message. Encrypted using the parent node's key. Signed using the address key.
 }
 
+// CreateFolderRes contains the result for a CreateFolder request.
 type CreateFolderRes struct {
-	ID string // Encrypted Link ID
+	ID string // The encrypted Link ID.
 }
