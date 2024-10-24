0e2d512cf08d81e6e3235f148bccdfca6316580f
ElectroNafta <atanas.janeshliev@proton.ch>
2024-10-24 12:13:52 +0200

    feat(IMEX-45): added unauth telemetry call

--- data.go
+++ data.go
@@ -2,6 +2,7 @@ package proton
 
 import (
 	"context"
+
 	"github.com/go-resty/resty/v2"
 )
 
@@ -15,4 +16,11 @@ func (c *Client) SendDataEventMultiple(ctx context.Context, req SendStatsMultiRe
 	return c.do(ctx, func(r *resty.Request) (*resty.Response, error) {
 		return r.SetBody(req).Post("/data/v1/stats/multiple")
 	})
+}
+
+func (m *Manager) SendUnauthDataEvent(ctx context.Context, req SendStatsReq) error {
+	if _, err := m.r(ctx).SetBody(req).Post("/data/v1/stats"); err != nil {
+		return err
+	}
+	return nil
 }
