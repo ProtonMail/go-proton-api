7525f013ddc1bb096fc80c9165c5a487d8d53d43
ElectroNafta <atanas.janeshliev@proton.ch>
2025-07-11 13:18:30 +0200

    feat(BRIDGE-396): extended observability metric type definition

--- observability_types.go
+++ observability_types.go
@@ -9,4 +9,6 @@ type ObservabilityMetric struct {
 	Version   int         `json:"Version"`
 	Timestamp int64       `json:"Timestamp"` // Unix timestamp
 	Data      interface{} `json:"Data"`
+
+	ShouldCache bool `json:"-"` // Internal field, indicating whether we should cache the given observability metric.
 }
